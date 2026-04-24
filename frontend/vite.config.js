e8045967e9d2a42107f2601ca8d2705c20cb9e73
Rishabh Puri <rishabhpuri205@gmail.com>
2026-04-24 17:14:40 +0530

    Railway Backend Depployment Fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,        // 👈 THIS IS THE KEY
-    port: 5173,
+    port: 3000,
+    host: true // exposes to 0.0.0.0, needed for Railway
   },
-});
+  preview: {
+    port: process.env.PORT || 3000,
+    host: true
+  }
+})
